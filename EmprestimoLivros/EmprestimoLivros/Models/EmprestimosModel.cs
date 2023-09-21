af2faaf8f68aa5db40f3602927223ad13f806597
Gustavo Kertes da Costa <gustavodacosta0@outlook.com>
2023-09-21 17:04:49 -0300

    Initial commit

---
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do recebedor!")]
+
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do fornecedor!")]
+
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do livro emprestado!")]
+        public string LivroEmprestado { get; set; }
+
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿
+
+
+$(document).ready(function () {
+
+    $('#Emprestimos').DataTable({
+        language: (
+            {
+                "decimal": "",
+                "emptyTable": "Não temos emprestimos cadastrados!",
+                "info": "Mostrando _START_ registro de _END_ em um total de _TOTAL_ resultados",
+                "infoEmpty": "Showing 0 to 0 of 0 entries",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ resultados",
+                "loadingRecords": "Loading...",
+                "processing": "",
+                "search": "Buscar:",
+                "zeroRecords": "Nenhum empréstimo foi encontrado!",
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Último",
+                    "next": "Próximo",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                }
+            }
+        )
+    });
+
+    setTimeout(function () {
+
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        })
+
+    }, 3000)
+});
